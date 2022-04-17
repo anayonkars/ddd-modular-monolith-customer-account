0061803261ab9dd52e82490e076f6c07d74727ad
Sandeep Jagtap <sandeep.jagtap@gmail.com>
2022-04-17 18:43:06 +0530

    Use accountcontroller updateAddress as in-process method call from Customer microservice to Account module included in same microservice

--- customer/src/main/java/com/ddd_bootcamp/threetier/applicationservice/CustomerAppService.java
+++ customer/src/main/java/com/ddd_bootcamp/threetier/applicationservice/CustomerAppService.java
@@ -3,11 +3,10 @@ package com.ddd_bootcamp.threetier.applicationservice;
 import com.ddd_bootcamp.domain.AccountId;
 import com.ddd_bootcamp.domain.Address;
 import com.ddd_bootcamp.domain.Customer;
-import com.ddd_bootcamp.threetier.controller.resource.AddressData;
+import com.ddd_bootcamp.threetier.controller.AccountController;
+import com.ddd_bootcamp.threetier.controller.viewModel.AddressRequest;
 import com.ddd_bootcamp.threetier.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,12 +16,15 @@ import java.util.stream.Collectors;
 public class CustomerAppService {
 
     private CustomerRepository customerRepository;
+    private AccountController accountController;
 
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
 
-    public CustomerAppService(CustomerRepository customerRepository) {
+    public CustomerAppService(CustomerRepository customerRepository, AccountController accountController) {
+
         this.customerRepository = customerRepository;
+        this.accountController = accountController;
     }
 
     //@Transactional
@@ -35,7 +37,6 @@ public class CustomerAppService {
     public Customer updateAddress(UUID customerId, Address address) {
         Customer customer = customerRepository.find(customerId);
         customer.updateAddress(address);
-        System.out.println("restTemplate = " + restTemplate);
         Customer savedCustomer = customerRepository.save(customer);
 
         List<String> accountIds = customer.getAccountIds().stream()
@@ -43,11 +44,11 @@ public class CustomerAppService {
         AddressRequest request = new AddressRequest();
         request.setCity(address.getCity());
         accountIds.forEach(accountId ->
-                restTemplate.put("http://localhost:8083/accounts/" + accountId + "/address",
-                        request, AccountResource.class));
+//                restTemplate.put("http://localhost:8083/accounts/" + accountId + "/address",
+//                        request, AccountResource.class));
+                accountController.updateAddress(request, accountId));
 
         return savedCustomer;
-
     }
 
     public Customer addAccount(UUID customerId, String accountId) {
@@ -55,56 +56,6 @@ public class CustomerAppService {
         customer.add(new AccountId(UUID.fromString(accountId)));
         customerRepository.save(customer);
         return customer;
-    }
-
-    class AddressRequest {
-        private String city;
-
-        //needed for spring framework
-        public AddressRequest() {
-        }
-
-        //needed for spring framework
-        public String getCity() {
-            return city;
-        }
-
-        //needed for spring framework
-        public void setCity(String city) {
-            this.city = city;
-        }
-
-        @Override
-        public String toString() {
-            return "AddressRequest{" +
-                    "city='" + city + '\'' +
-                    '}';
-        }
-
-    }
-
-    class AccountResource {
-        private UUID accountId;
-        private AddressData addressData;
-
-        public AccountResource() {
-        }
-
-        public UUID getAccountId() {
-            return accountId;
-        }
-
-        public void setAccountId(UUID accountId) {
-            this.accountId = accountId;
-        }
-
-        public AddressData getAddressData() {
-            return addressData;
-        }
-
-        public void setAddressData(AddressData addressData) {
-            this.addressData = addressData;
-        }
     }
 
 }
